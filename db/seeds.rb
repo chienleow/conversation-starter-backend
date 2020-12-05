9927379b01fca08a5caf42ea55422ab1e57502dc
chienleow <chienleow1@gmail.com>
2020-12-04 23:44:50 -0500

    user model, migration, seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+rachel = User.create(username: "Rachel")
+monica = User.create(username: "Monica")
+phoebe = User.create(username: "Phoebe")
