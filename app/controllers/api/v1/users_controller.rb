93136a7249cb0a4f11fb9b8d5b994243b21b0150
chienleow <chienleow1@gmail.com>
2020-12-06 11:14:50 -0500

    create method for user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::UsersController < ApplicationController
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            @token = encode_token(user_id: @user.id)
+            render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+        else
+            render json: { error: 'Failed to create user, try again.' }, status: :not_acceptable
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
+    end
 end
