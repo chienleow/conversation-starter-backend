d090d5f310491829b896f87e26cfd72693352cb0
chienleow <chienleow1@gmail.com>
2020-12-19 22:50:23 -0500

    add @ to question in create method in questions controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -5,18 +5,17 @@ class Api::V1::QuestionsController < ApplicationController
     end
 
     def create
-        question = Question.new(question_params)
-        if question.save
-            render json: question, status: :accepted
+        @question = Question.new(question_params)
+        if @question.save
+            render json: @question, status: :accepted
         else
-            render json: { errors: question.errors.full_messages }, status: :unprocessable_entity
+            render json: { errors: @question.errors.full_messages }, status: :unprocessable_entity
         end
     end
 
     private
     
     def question_params
-        ## update params to accept user attribute
         params.require(:question).permit(:question, :user_id)
     end
 end
