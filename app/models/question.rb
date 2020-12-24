991d9281cf2d6af5219b7dfca247762972318b80
chienleow <chienleow1@gmail.com>
2020-12-24 13:56:51 -0500

    remove some comments

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,3 @@
 class Question < ApplicationRecord
     belongs_to :user
-    # accepts_nested_attributes_for :user
-    ## nested attributes for new user in question params
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
 class User < ApplicationRecord
     has_many :questions
-    # accepts_nested_attributes_for :questions
 end
